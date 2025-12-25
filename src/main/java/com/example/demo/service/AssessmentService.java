4339537c0824ed3b2922d1d6e73638333ed0b601
bala-amypo <bala@amypo.in>
2025-12-25 16:49:25 +0000

    User commit

--- src/main/java/com/example/demo/service/AssessmentService.java
+++ src/main/java/com/example/demo/service/AssessmentService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.AssessmentResult;
 
+@bean
 public interface AssessmentService {
 
     AssessmentResult recordAssessment(AssessmentResult result);
