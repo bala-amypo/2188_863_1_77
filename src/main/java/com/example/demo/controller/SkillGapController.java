c099193eee672192270670e5798f790363f3e2a0
bala-amypo <bala@amypo.in>
2025-12-27 15:02:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillGapController.java
+++ src/main/java/com/example/demo/controller/SkillGapController.java
@@ -1,9 +1,8 @@
-
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/skills")
-public class SkillController {
+@RequestMapping("/api/gaps")
+public class SkillGapController {
 }
